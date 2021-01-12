3d7ff990080b30e4b449bbf6fa9b4fcf35dbefce
ajsnow <ajsnow56@gmail.com>
2021-01-11 19:22:16 -0700

    JawsDB added to connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgz_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.connectConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    // port: 3306,
+    user: "root",
+    password: "",
+    database: "burgz_db"
+  });
+
+};
+
 
 // Make connection.
 connection.connect(function(err) {
@@ -17,6 +24,6 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
-
+connection.connect();
 // Export connection for our ORM to use.
 module.exports = connection;
